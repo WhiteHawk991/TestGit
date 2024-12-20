4f8af9d35a75431998634047e5925c51dbe70e08
WhiteHawk991 <Hawks1299@gmail.com>
2024-12-20 16:20:17 +0300

    Новый проект

---
+++ ConsoleApp1TestGit/ConsoleApp1TestGit/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApp1TestGit
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Как Вас зовут?");
+			string name = Console.ReadLine();
+			Console.WriteLine("Сколько Вам лет?");
+			string age = Console.ReadLine();
+			Greet(name, age);
+			Console.WriteLine("Рад был познакомиться!");
+			Console.ReadKey();
+
+		}
+
+		static void Greet(string name, string age)
+		{
+			Console.WriteLine("Здравствуйте, {0}", name);
+			Console.WriteLine("Мне сообщили, что Вам {0}", age);
+		}
+	}
+}
